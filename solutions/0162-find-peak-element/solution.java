f9498b85542509c46ac9a2b5ada4b151b02acaf6
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2024-08-08 15:51:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.5 MB (100.00%)

---
+++ solutions/0162-find-peak-element/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int max=nums[0],k=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>max)
+            {
+                max=nums[i];k=i;
+            }
+        }
+        return k;
+    }
+}
