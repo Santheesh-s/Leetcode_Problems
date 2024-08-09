f5b5a44373f8fecebedbbb45da082a1349b55314
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2024-08-09 09:54:17 +0000

    Sync LeetCode submission Runtime - 5 ms (12.79%), Memory - 42.3 MB (99.94%)

---
+++ solutions/0067-add-binary/solution.java
@@ -0,0 +1,9 @@
+import java.math.BigInteger;
+class Solution {
+    public String addBinary(String a, String b) {
+        BigInteger s1 = new BigInteger(a, 2);
+        BigInteger s2 = new BigInteger(b, 2);
+        BigInteger sum=s1.add(s2);
+        return sum.toString(2);
+    }
+}
