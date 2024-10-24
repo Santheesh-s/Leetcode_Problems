1bedcd6905b47eaa5067804f446027d138f50dba
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2024-10-24 14:52:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.7 MB (99.96%)

---
+++ solutions/1013-fibonacci-number/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fib(int n) {
+        int a=0,b=1,fib=1,i=1;
+        if(n==0)
+            return 0;
+        if(n<=2)
+            return 1;
+        while(i<n-1)
+        {
+            a=b;
+            b=fib;
+            fib=a+b;
+            i++;
+        }
+        return fib;
+    }
+}
