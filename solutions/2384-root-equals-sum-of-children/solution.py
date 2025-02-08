c89318f642a07f311ee27c3e4d91f768b93c31ea
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-02-08 10:44:50 +0000

    Sync LeetCode submission Runtime - 2 ms (3.85%), Memory - 12.3 MB (81.54%)

---
+++ solutions/2384-root-equals-sum-of-children/solution.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def checkTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        sum=root.left.val
+        ch=root.val
+        sum+=root.right.val
+        print(sum)
+        return ch==sum
