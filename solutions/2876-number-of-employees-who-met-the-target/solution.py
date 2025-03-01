6ea7a1a503689eb816251a57f15d69602b3b2497
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-03-01 10:33:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (84.09%)

---
+++ solutions/2876-number-of-employees-who-met-the-target/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def numberOfEmployeesWhoMetTarget(self, hours, target):
+        """
+        :type hours: List[int]
+        :type target: int
+        :rtype: int
+        """
+        return len([i for i in hours if i>=target])
