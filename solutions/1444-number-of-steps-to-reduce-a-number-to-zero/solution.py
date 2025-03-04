b6ca49d6a7fb77b2ee8018ae608183327b18f609
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-03-04 15:58:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (13.16%)

---
+++ solutions/1444-number-of-steps-to-reduce-a-number-to-zero/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numberOfSteps(self, n):
+        """
+        :type num: int
+        :rtype: int
+        """
+        count=0
+        while(n!=0):
+            if n%2==0:
+                n=n/2
+            else:
+                n-=1
+            count+=1
+        return count
