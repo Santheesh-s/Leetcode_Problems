8470b88dba57550cb2b693717f429d69a22eeae6
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-03-15 09:12:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.2 MB (99.19%)

---
+++ solutions/3582-find-indices-of-stable-mountains/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def stableMountains(self, height, threshold):
+        """
+        :type height: List[int]
+        :type threshold: int
+        :rtype: List[int]
+        """
+        ls=[]
+        for i in range(1,len(height)):
+            if height[i-1]>threshold:
+                ls.append(i)
+        return ls
