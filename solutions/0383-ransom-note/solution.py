acdf571f63fa1185018ff737e85250a31977e6c6
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-03-25 10:00:05 +0000

    Sync LeetCode submission Runtime - 23 ms (85.16%), Memory - 12.6 MB (80.72%)

---
+++ solutions/0383-ransom-note/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        magazine=list(magazine)
+        ransomNote=list(ransomNote)
+        for i in ransomNote:
+            if i not in magazine:
+                return False
+            else:
+                magazine.remove(i)
+        return True
