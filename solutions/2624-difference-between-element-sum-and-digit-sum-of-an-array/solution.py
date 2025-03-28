b5f8126c32a15d691d443494bd07e637b3f83a18
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-03-28 14:00:18 +0000

    Sync LeetCode submission Runtime - 55 ms (48.46%), Memory - 12.6 MB (53.95%)

--- solutions/2624-difference-between-element-sum-and-digit-sum-of-an-array/solution.py
+++ solutions/2624-difference-between-element-sum-and-digit-sum-of-an-array/solution.py
@@ -4,10 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        sum1=sum(nums)
-        s=''.join(str(x) for x in nums)
-        s2=(list(s))
-        sum2=sum(int(x) for x in s2)
-        print(sum2)
-        return abs(sum1-sum2)
-        
+        ls=sum(nums)
+        s=""
+        for i in nums:
+            s+=str(i)
+        n=0
+        for i in s:
+            n+=int(i)
+        return abs(ls-n)
