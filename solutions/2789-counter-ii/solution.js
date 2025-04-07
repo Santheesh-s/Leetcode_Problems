1f664b032754fd30df5a0b2e0b1faf1157784f6f
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-04-07 14:16:40 +0000

    Sync LeetCode submission Runtime - 44 ms (87.87%), Memory - 58.3 MB (5.43%)

---
+++ solutions/2789-counter-ii/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    var n=init;
+    return {increment:function(){return ++init;},
+    reset:function() {init=n;return n},
+    decrement:function() {return --init}}
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
