d47d42c5f123ff6369c0ede87450354dbe36aa26
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-04-16 09:21:11 +0000

    Sync LeetCode submission Runtime - 20 ms (23.01%), Memory - 12.5 MB (15.04%)

--- solutions/1806-count-of-matches-in-tournament/solution.py
+++ solutions/1806-count-of-matches-in-tournament/solution.py
@@ -1,7 +1,3 @@
 class Solution(object):
     def numberOfMatches(self, n):
-        ls=[]
-        while(n//2>=1):
-            ls.append(n//2)
-            n-=n//2
-        return sum(ls)
+        return n-1;
