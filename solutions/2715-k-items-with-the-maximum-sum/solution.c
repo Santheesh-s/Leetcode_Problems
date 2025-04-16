c03398b2b7aae097db22e8112ec61f72b3867638
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-04-16 09:11:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.5 MB (67.80%)

---
+++ solutions/2715-k-items-with-the-maximum-sum/solution.c
@@ -0,0 +1,15 @@
+int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        int n=numOnes+numZeros+numNegOnes;
+        int i,sum=0;
+        int m=numOnes+numZeros;
+        int arr[n+1];
+        for(i=0;i<numOnes;i++)
+            arr[i]=1;
+        for(i=numOnes;i<m;i++)
+            arr[i]=0;
+        for(i=m;i<n;i++)
+            arr[i]=-1;
+        for(i=0;i<k;i++)
+            sum+=arr[i];
+        return sum;
+}
