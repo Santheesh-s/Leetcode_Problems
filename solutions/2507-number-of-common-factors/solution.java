fa1018c91a8346b14d84d4965a42c64efbb9729d
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-04-17 09:23:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.5 MB (100.00%)

--- solutions/2507-number-of-common-factors/solution.java
+++ solutions/2507-number-of-common-factors/solution.java
@@ -1,7 +1,11 @@
 class Solution {
     public int commonFactors(int a, int b) {
-        int n=0;
-        for(int i=1;i<=b;i++)
+        int n=0,m;
+        if(a<b)
+            m=a;
+        else
+            m=b;
+        for(int i=1;i<=m;i++)
             if(a%i==0 && b%i==0)
                 n++;
         return n;
