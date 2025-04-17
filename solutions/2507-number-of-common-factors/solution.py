9a8fb146f7bad152e0024c1337fb377cbab0e9f9
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-04-17 09:16:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (16.79%)

---
+++ solutions/2507-number-of-common-factors/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def commonFactors(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        n=0;
+        for i in range(1,b+1):
+            if(a%i==0 and b%i==0):
+                n+=1;
+        return n;
