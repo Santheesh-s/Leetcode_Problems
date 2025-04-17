2f1b3d3296d4343e475b010e6021d4ab3f4f1dc5
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-04-17 07:59:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.7 MB (72.59%)

---
+++ solutions/1406-subtract-the-product-and-sum-of-digits-of-an-integer/solution.c
@@ -0,0 +1,10 @@
+int subtractProductAndSum(int n) {
+    int temp=n,p=1,s=0;
+        while(temp!=0)
+        {
+            p*=temp%10;
+            s+=temp%10;
+            temp/=10;
+        }
+        return p-s;
+}
