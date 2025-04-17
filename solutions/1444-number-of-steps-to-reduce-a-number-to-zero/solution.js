f15882d70a6c9a1ebb07b99ad8f373bcd1e26759
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-04-17 08:07:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 53.2 MB (91.58%)

---
+++ solutions/1444-number-of-steps-to-reduce-a-number-to-zero/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(n) {
+    var count=0;
+        while(n!=0)
+        {    
+            if(n%2==0)
+                n=n/2;
+            else
+                n-=1;
+            count+=1;
+        }
+        return count;
+};
