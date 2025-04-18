d7314802a9e2b5b5f63a87d1f5cc9e855705310b
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-04-18 05:01:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 57 MB (20.84%)

---
+++ solutions/0231-power-of-two/solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if(n>=1)
+        {
+            if((n&(n-1))==0)
+                return true;
+        }
+        return false;
+};
