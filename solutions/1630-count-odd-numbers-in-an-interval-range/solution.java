1823bdda5178e60d4c4e33c056721a1eeee286fc
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-04-18 06:14:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.4 MB (99.99%)

--- solutions/1630-count-odd-numbers-in-an-interval-range/solution.java
+++ solutions/1630-count-odd-numbers-in-an-interval-range/solution.java
@@ -1,19 +1,11 @@
 class Solution {
     public int countOdds(int low, int high) {
-        if (low % 2 == 0) {
-            low++;
-        }
-
-        // If high is odd, include it in the count, else end at the previous odd number
-        if (high % 2 == 0) {
-            high--;
-        }
-
-        // Calculate the number of odd numbers between low and high inclusive
-        if (low > high) {
-            return 0; // No odd numbers in the range
-        }
-
-        return (high - low) / 2 + 1;
+    if (low % 2 == 0)
+        low++;
+    if (high % 2 == 0)
+        high--;
+    if (low > high)
+        return 0;
+    return (high - low) / 2 + 1; 
     }
 }
