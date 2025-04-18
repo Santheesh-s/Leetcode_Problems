250dd6f359ef218e54b7fbab630de1e004c2935d
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-04-18 08:42:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.5 MB (100.00%)

---
+++ solutions/0461-hamming-distance/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count=0,r=x^y;
+    while(r>0)
+    {
+        if((r&1)==1)
+            count++;
+        r>>=1;
+    }
+    return count;
+    }
+}
