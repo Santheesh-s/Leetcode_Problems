0f57af7bb1f9614d6499b44822a10cedcdc87f5c
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-04-18 03:41:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.2 MB (44.70%)

--- solutions/0009-palindrome-number/solution.c
+++ solutions/0009-palindrome-number/solution.c
@@ -1,31 +1,13 @@
 bool isPalindrome(int x) {
-    if(x<0)
+    int temp=x;
+    long int rev=0;
+    if(x<0 || (x!=0 && x%10==0)) return false;
+    while(x!=0)
     {
-        return false;
+        rev=rev*10+x%10;
+        x/=10;
     }
-    else
-    {
-        int n=x,rem,rev=0;
-        while(n!=0)
-        {
-            rem=n%10;
-            n=n/10;
-        if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && rem > INT_MAX % 10)) {
-            return 0; // Overflow, return 0
-        }
-        if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && rem < INT_MIN % 10)) {
-            return 0; // Underflow, return 0
-        }
-        
-         rev=rev*10+rem;
-        }
-        if(rev==x)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    if(rev==temp)
+        return true;
+    return false;
 }
