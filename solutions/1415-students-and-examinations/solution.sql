90db2e819922553dde1ea0d00489f9df4d1c0ed1
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-06-10 02:52:29 +0000

    Sync LeetCode submission Runtime - 915 ms (56.46%), Memory - 0.0B (100.00%)

---
+++ solutions/1415-students-and-examinations/solution.sql
@@ -0,0 +1,12 @@
+SELECT 
+  S.student_id,
+  S.student_name,
+  Sub.subject_name,
+  COUNT(E.subject_name) AS attended_exams
+FROM Students S
+CROSS JOIN Subjects Sub
+LEFT JOIN Examinations E
+  ON E.student_id = S.student_id AND E.subject_name = Sub.subject_name
+GROUP BY 
+  S.student_id, S.student_name, Sub.subject_name ORDER BY S.student_id,Sub.subject_name;
+
