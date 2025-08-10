333fead93f74aa6242cb745b56024117f3b1c5e9
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-08-10 17:35:15 +0000

    Sync LeetCode submission Runtime - 197 ms (32.98%), Memory - 59 MB (19.37%)

---
+++ solutions/1354-find-players-with-zero-or-one-losses/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findWinners(self, matches):
+        """
+        :type matches: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        winners, losers, table = [], [], {}
+        for winner, loser in matches:
+            table[winner] = table.get(winner, 0) 
+            table[loser] = table.get(loser, 0) + 1
+        for k, v in table.items():
+            if v == 0:
+                winners.append(k) 
+            if v == 1:
+                losers.append(k)
+        return [sorted(winners), sorted(losers)]
+        
