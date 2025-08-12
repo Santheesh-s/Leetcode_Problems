798e42425e8af685932f73d43a881f128cc11571
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-08-12 15:26:02 +0000

    Sync LeetCode submission Runtime - 5 ms (50.00%), Memory - 12.5 MB (58.97%)

---
+++ solutions/2754-maximum-strength-of-a-group/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxStrength(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total = 1
+        positive = [num for num in nums if num > 0]
+        negative = [num for num in nums if num < 0]
+        if len(negative) % 2 != 0:
+            negative.sort()
+            negative.pop()
+        if not positive and not negative:
+            return max(nums)
+        for num in positive:
+            total *= num
+        for num in negative:
+            total *= num
+        return total
+        
