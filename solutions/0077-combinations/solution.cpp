0f38b9bd8a7f8d7a41f1ab7c0a480a2be98019b6
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-09-24 05:26:54 +0000

    Sync LeetCode submission Runtime - 35 ms (90.53%), Memory - 62.6 MB (67.89%)

---
+++ solutions/0077-combinations/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void backtrack(int n,int k,vector<vector<int>>&res,vector<int>&combi,int cur)
+    {
+        if(combi.size()==k)
+        {
+            res.push_back(combi);
+            return ;
+        }
+        for(int i=cur;i<=n;i++)
+        {
+            combi.push_back(i);
+            backtrack(n,k,res,combi,i+1);
+            combi.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>res;
+        vector<int>combi;
+        backtrack(n,k,res,combi,1);
+        return res;
+    }
+};
