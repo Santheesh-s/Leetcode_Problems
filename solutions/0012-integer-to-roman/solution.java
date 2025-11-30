368cb5465fd55ab9877ba35892f197c55ac36b9a
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-11-30 09:07:18 +0000

    Sync LeetCode submission Runtime - 4 ms (34.09%), Memory - 46.5 MB (47.55%)

--- solutions/0012-integer-to-roman/solution.java
+++ solutions/0012-integer-to-roman/solution.java
@@ -1,19 +1,19 @@
 class Solution {
-    public String intToRoman(int num) {
-        int n[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String s[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        String str=new String();
+    private static final int[] arr={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    private static String[] brr={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        
+    public static String intToRoman(int num) {
         int i=0;
+        String s="";
         while(num>0)
         {
-            if(num>=n[i])
+            if(num-arr[i]>=0)
             {
-                str+=s[i];
-                num=num-n[i];
+                s+=brr[i];
+                num-=arr[i];
             }
-            else
-                i+=1;
+            else i++;
         }
-        return str;
+        return s;
     }
 }
