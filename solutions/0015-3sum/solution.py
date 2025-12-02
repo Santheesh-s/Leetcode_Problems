16ca20903eb01ae68d2b4a4ecf1f80d40820d852
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-02 15:46:41 +0000

    Sync LeetCode submission Runtime - 743 ms (52.37%), Memory - 18.4 MB (41.75%)

--- solutions/0015-3sum/solution.py
+++ solutions/0015-3sum/solution.py
@@ -5,21 +5,24 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums=sorted(nums)
-        n=len(nums);
+        n=len(nums)
         ls=[]
-        for i in range(0,n-2):
-            if(i>0 and nums[i]==nums[i-1]):
-                continue;
+        for i in range(n-2):
+            if(i>0 and nums[i]==nums[i-1]): continue
             left=i+1
-            right=n-1;
+            right=n-1
             while(left<right):
-                sum=nums[left]+nums[right]+nums[i];
-                if(sum==0):
-                    ls.append((nums[left],nums[right],nums[i]));
-                    left+=1;
-                    right-=1;
-                elif(sum>0):
+                s=nums[left]+nums[right]+nums[i]
+                if(s==0):
+                    ls.append([nums[left],nums[right],nums[i]])
+                    left_val = nums[left]
+                    right_val = nums[right]
+                    while left < right and nums[left] == left_val:
+                        left += 1
+                    while left < right and nums[right] == right_val:
+                        right -= 1
+                elif s>0:
                     right-=1
                 else:
                     left+=1
-        return [list(t) for t in set(ls)]
+        return ls
