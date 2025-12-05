0ef9a429a40fa028bb98d658c304308931ddb9d0
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-05 14:15:49 +0000

    Sync LeetCode submission Runtime - 1 ms (74.86%), Memory - 46.8 MB (45.72%)

--- solutions/0026-remove-duplicates-from-sorted-array/solution.java
+++ solutions/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,16 +1,9 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i=0;
-        if(nums.length==0)
-            return 0;
-        for(int j=1;j<nums.length;j++)
-        {
-            if(nums[i]!=nums[j])
-            {
-                i++;
-                nums[i]=nums[j];
-            }
-        }
-        return i+1;
+        int write=1,n=nums.length;
+        for(int i=1;i<n;i++)
+            if(nums[i]!=nums[i-1])
+                nums[write++]=nums[i];
+        return write;
     }
 }
