3a537dabaf6f5d08f7e9784e1b315b5815d773df
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-14 07:04:50 +0000

    Sync LeetCode submission Runtime - 1 ms (22.94%), Memory - 12.5 MB (59.02%)

--- solutions/0118-pascals-triangle/solution.py
+++ solutions/0118-pascals-triangle/solution.py
@@ -1,14 +1,17 @@
 class Solution(object):
-    def generate(self, numRows):
+    def __init__(self):
+        i=2
+        self.ls=[[1],[1,1]]
+        while(i<31):
+            a=[1]
+            for j in range(len(self.ls[i-1])-1):
+                a.append(self.ls[i-1][j+1]+self.ls[i-1][j])
+            a.append(1)
+            self.ls.append(a)
+            i+=1
+    def generate(self, n):
         """
         :type numRows: int
         :rtype: List[List[int]]
         """
-        ls=[[1]]
-        for i in range(0,numRows-1):
-            s=[1]
-            for j in range(len(ls[i])-1):
-                s+=[ls[i][j]+ls[i][j+1]]
-            s+=[1]
-            ls.append(s)
-        return ls
+        return self.ls[:n]
