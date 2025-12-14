3f298ebc013950051e411174a69048b8a829ce42
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-14 06:13:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (53.61%)

--- solutions/0263-ugly-number/solution.py
+++ solutions/0263-ugly-number/solution.py
@@ -6,14 +6,13 @@ class Solution(object):
         """
         while(n>1):
             if(n%2==0):
-                n/=2;
+                n/=2
             elif(n%3==0):
-                n/=3;
+                n/=3
             elif(n%5==0):
-                n/=5;
+                n/=5
             else:
-                return False;
-        if(n==1):
-            return True;
-        return False;
-        
+                return False
+        if n==1:
+            return True
+        return False
