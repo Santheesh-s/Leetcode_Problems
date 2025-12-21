3f7f713af8b1ff863ed478883f103810ccaa436a
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-21 15:58:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 45.2 MB (14.81%)

--- solutions/0081-search-in-rotated-sorted-array-ii/solution.java
+++ solutions/0081-search-in-rotated-sorted-array-ii/solution.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        for(int n:nums)
-        {
-            if(n==target)
+        if(target<=nums[nums.length-1])
+            for(int i=nums.length-1;i>=0 && nums[i]>=target;i--)
+                if(nums[i]==target)
+                    return true;
+        for(int i=0;i<nums.length && nums[i]<=target;i++)
+            if(nums[i]==target)
                 return true;
-        }
         return false;
     }
 }
