4bce5135ad4171d2c1b5c09de6bb17c31c78c1b0
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-23 15:37:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.1 MB (73.66%)

--- solutions/1585-the-kth-factor-of-n/solution.java
+++ solutions/1585-the-kth-factor-of-n/solution.java
@@ -1,14 +1,15 @@
-class Solution {
-    public int kthFactor(int n, int k) {
-        int arr[]=new int[n],kt=0;
-        for(int i=1;i<=n;i++)
-        {
-            if(n%i==0)
+class Solution 
+{
+    int kthFactor(int n, int k) 
+    {
+        int count = 0;
+        for(int i = 1; i <= n; i++)
+            if(n % i == 0)
             {
-                arr[kt++]=i;
+                count++;
+                if(count == k)
+                    return i;
             }
-        }
-        if(k>kt)return -1;
-        return arr[k-1];
+        return -1;
     }
 }
