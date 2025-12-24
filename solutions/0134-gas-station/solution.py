8b0ae64e3d8086cc983199f6e7bc4ff43d454dbe
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-24 16:10:57 +0000

    Sync LeetCode submission Runtime - 19 ms (96.49%), Memory - 18.1 MB (32.88%)

---
+++ solutions/0134-gas-station/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        if sum(gas)<sum(cost):
+            return -1;
+        cur=0
+        start=0
+        for i in range(len(gas)):
+            cur+=gas[i]-cost[i]
+            if(cur<0):
+                start=i+1
+                cur=0
+        return start
