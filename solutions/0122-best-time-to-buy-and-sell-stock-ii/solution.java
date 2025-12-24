31ae166a3058e16ce451a3a46e7002e84fed7eed
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-24 15:02:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 45.8 MB (18.42%)

--- solutions/0122-best-time-to-buy-and-sell-stock-ii/solution.java
+++ solutions/0122-best-time-to-buy-and-sell-stock-ii/solution.java
@@ -1,12 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min=prices[0],profit=0,i=0;
-        while(i<prices.length)
+        int min=prices[0],profit=0;
+        for(int i=0;i<prices.length;i++)
         {
             if(prices[i]>min)
                 profit+=prices[i]-min;
             min=prices[i];
-            i++;
         }
         return profit;
     }
