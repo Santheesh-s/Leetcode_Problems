f18c264fc1867d37a5e75a67f2773ca993c4fc81
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-24 16:58:27 +0000

    Sync LeetCode submission Runtime - 1 ms (35.19%), Memory - 43.2 MB (20.69%)

--- solutions/0058-length-of-last-word/solution.java
+++ solutions/0058-length-of-last-word/solution.java
@@ -1,12 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int i=0;
-        for(i=s.length()-1;i>=0 && s.charAt(i)==' ';i--);
-        int l=0;
-        if(i>=0)
-            while(i>=0 && s.charAt(i)!=' ')
-            {    l++;i--; }
-        
-        return l;
+        String[] arr=s.split(" ");
+        return arr[arr.length-1].length();
     }
 }
