f3d972ff221fe2b62a0700f138c52c13cc4cc325
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-24 15:46:28 +0000

    Sync LeetCode submission Runtime - 2 ms (94.22%), Memory - 71.8 MB (58.06%)

--- solutions/0238-product-of-array-except-self/solution.java
+++ solutions/0238-product-of-array-except-self/solution.java
@@ -1,20 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int pro=1,z=0;
-        for(int i:nums)
+        // int ans[]=new int[nums.length];
+        // for(int i=1;i<nums.length;i++)
+        //     nums[i]*=nums[i-1];
+        int p=1,z=0;
+        for(int i=0;i<nums.length;i++)
         {
-            if(i==0)
+            if(nums[i]==0)
                 z++;
             else
-                pro*=i;
+                p*=nums[i];
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (z > 1)
-                nums[i] = 0;
-            else if (z == 1) 
-                nums[i] = (nums[i] == 0) ? pro : 0;
-            else 
-                nums[i] = pro / nums[i];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(z>1)
+                nums[i]=0;
+            else if(z==1)
+                nums[i]=(nums[i]!=0?0:p);
+            else
+                nums[i]=p/nums[i];
         }
         return nums;
     }
