04a6ebb7292a5e5cd83456912763ac7d3ddf1639
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-25 14:08:33 +0000

    Sync LeetCode submission Runtime - 1 ms (77.73%), Memory - 42.7 MB (22.73%)

---
+++ solutions/0202-happy-number/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    
+public int split(int n)
+{
+    int sum=0,mod;
+    while(n!=0)
+    {
+        mod=n%10;
+        sum+=(mod*mod);
+        n/=10;
+    }
+    return sum;
+}
+public boolean isHappy(int n) 
+{  
+    int fast=n,slow=n;
+    while(fast!=1)
+    {
+        slow=split(slow);
+        fast=split(split(fast));
+        if(fast==1 || slow==1)
+            return true;
+        if(slow==fast)
+            return false;
+    }
+    return true;
+}
+}
+
+
+
