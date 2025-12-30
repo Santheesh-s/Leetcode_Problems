37213e51bd3a45171c4001468478a0cd2d780394
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-30 16:16:59 +0000

    Sync LeetCode submission Runtime - 192 ms (68.18%), Memory - 24 MB (19.70%)

---
+++ solutions/0982-minimum-increment-to-make-array-unique/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minIncrementForUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums=sorted(nums)
+        res=0
+        for i in range(1,len(nums)):
+            if nums[i]<=nums[i-1]:
+                res+=nums[i-1]-nums[i]+1
+                nums[i]=nums[i-1]+1
+        return res;
+
