6fa11421074a9ff63b3804a62edcd314dc26c278
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-31 14:28:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.3 MB (11.43%)

---
+++ solutions/0191-number-of-1-bits/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int a=0;
+        while(n!=0)
+        {
+            a+=(n&1);
+            n>>=1;
+        }
+        return a;
+    }
+};
