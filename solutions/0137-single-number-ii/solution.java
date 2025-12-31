b96d49525cb33c6661adec0932b67e7cb05bb370
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-31 14:37:30 +0000

    Sync LeetCode submission Runtime - 6 ms (41.47%), Memory - 46.7 MB (5.96%)

---
+++ solutions/0137-single-number-ii/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int a:nums)
+            map.put(a,map.getOrDefault(a,0)+1);
+        for(Map.Entry<Integer,Integer> entry:map.entrySet())
+            if(entry.getValue()==1)
+                return entry.getKey();
+        return 0;
+    }
+}
