30f255e50975e478331f396ed37827a06aba8c83
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-31 15:17:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.4 MB (25.45%)

---
+++ solutions/0033-search-in-rotated-sorted-array/solution.c
@@ -0,0 +1,12 @@
+int search(int* nums, int numsSize, int target) {
+    int l=0,r=numsSize-1;
+    while(l<=r)
+    {
+        if(nums[l]==target)
+            return l;
+        if(nums[r]==target)
+            return r;
+        l++;r--; 
+    }
+    return -1;
+}
