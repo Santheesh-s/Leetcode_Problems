23e896990a48bb26cf656c453f921e6bcde0fb15
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2025-12-31 14:53:03 +0000

    Sync LeetCode submission Runtime - 4 ms (29.44%), Memory - 8.1 MB (71.65%)

---
+++ solutions/0190-reverse-bits/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseBits(int n) 
+    {
+        int a=0,i=0;
+        while(i<32)
+        {
+            a<<=1;
+            a|=(n&1);
+            n>>=1;
+            i++;
+        }
+        return a;
+    }
+};
