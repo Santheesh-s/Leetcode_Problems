252b742c709e181016d1f188a9c90f9d9402659c
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2026-01-01 05:57:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.6 MB (10.96%)

--- solutions/0066-plus-one/solution.c
+++ solutions/0066-plus-one/solution.c
@@ -1,9 +1,9 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* plusOne(int* digits, int n, int* returnSize) {
-    *returnSize=n;
-    for(int i=n-1;i>=0;i--)
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    *returnSize=digitsSize;
+    for(int i=digitsSize-1;i>=0;i--)
     {
         if(digits[i]<9)
         {
@@ -12,8 +12,9 @@ int* plusOne(int* digits, int n, int* returnSize) {
         }
         digits[i]=0;
     }
-    *returnSize=n+1;
-    int *arr=(int*)calloc(n+1,sizeof(int));
+    *returnSize+=1;
+    int *arr=(int*)calloc(digitsSize+1,sizeof(int));
     arr[0]=1;
     return arr;
+
 }
